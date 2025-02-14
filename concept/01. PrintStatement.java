13ff4b3c75500b21ff6ca6abf7136e80dccb789a
Saad <reach.saad@outlook.com>
2025-02-14 17:51:03 +0530

    Create 01. PrintStatement.java

---
+++ concept/01. PrintStatement.java
@@ -0,0 +1,11 @@
+//import java.io.*
+// IMPORTANT: class name must be same as file name.
+// EG: FILE.java then 
+// class FILE {
+// }
+  
+class PrintStatement {
+  public static void main (String args[]) {
+    System.out.println("This is my first java program.");
+  }
+}
